319acaa79d8236f844028532182132e99305d263
Antonio C Neto <antoniocavalcante1910@hotmail.com>
2024-08-27 17:33:43 -0300

    fix(ETA) fix jets configure

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,4 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  // moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  // transform: {
-  //   '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-  // },
+  setupFilesAfterEnv: ['@testing-library/jest-dom']
 };
