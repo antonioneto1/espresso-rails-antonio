84a11ba78c7890b5d92d13c73f2d8cabbced84dd
Antonio C Neto <antoniocavalcante1910@hotmail.com>
2024-08-27 17:58:27 -0300

    fix(ETA) fix coverageReporters

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
   testPathIgnorePatterns: ['/vendor/'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'json', 'text'],
 };
